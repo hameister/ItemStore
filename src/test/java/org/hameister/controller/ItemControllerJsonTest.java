37316f9f9b2930bbc3f653f961450ce55126cffb
hameister <joern@hameister.org>
2018-05-31 14:01:57 +0200

    added Elasticsearch.

--- src/test/java/org/hameister/controller/ItemControllerJsonTest.java
+++ src/test/java/org/hameister/controller/ItemControllerJsonTest.java
@@ -1,34 +1,16 @@
 package org.hameister.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hameister.ItemStoreApplication;
-import org.hameister.elastic.ElasticConfiguration;
 import org.hameister.model.Item;
-import org.hameister.service.ItemService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.any;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.mock;
 
 /**
  * Created by hameister on 16.01.17.
